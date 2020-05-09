320d2512144d673e7e479f1263a9f8ee8e9f9179
deeptika <deeptika24@gmail.com>
2020-05-09 21:59:00 +0530

    Simulated displaying of blog posts using dummy data

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author':'Srinidhi Kannan',
+        'title':'Travelling around the world',
+        'content':'First blog post',
+        'date_posted':'October 1, 2019'
+    },
+    {
+        'author':'Akshara Ramesh',
+        'title':'Netflix shows to watch',
+        'content':'Second blog post',
+        'date_posted':'September 13, 2020'
+    }
+]
+
 # Create your views here.
 def home(request):
-    return render(request,'blog/home.html')
+    context = {'posts':posts}
+    return render(request,'blog/home.html',context)
 
 def about(request):
     return render(request,'blog/about.html')
