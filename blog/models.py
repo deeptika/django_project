d631bc81ff1edd138ec20c1c66151c281cd1ece1
deeptika <deeptika24@gmail.com>
2020-05-10 19:40:52 +0530

    Created posts table

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+#0001_initial.py
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default = timezone.now)  #auto_now = True, auto_now_add = True
+    author = models.ForeignKey(User, on_delete = models.CASCADE)    #user deleted, posts deleted
+
+
